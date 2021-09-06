6087c7e8d4fe7fe7771531107b4388cf56b984c4
Gábor Tamás Tornai <gabo0127@edu.easj.dk>
2021-09-06 22:11:48 +0200

    First task

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+        private string _name;
+        private int _birthMonth;
+        private int _birthday;
+
+        public Student(string name, int birthMonth, int birthday)
+        {
+            name = _name;
+            birthMonth = _birthMonth;
+            birthday = _birthday;
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { value = _name; }
+        }
+
+        public int BithMonth
+        {
+            get { return _birthMonth; }
+            set { value = _birthMonth; }
+        }
+
+        public int Birthday
+        {
+            get { return _birthday; }
+            set { value = _birthday; }
+        }
+
+    }
+}
