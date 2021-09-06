e20733e3d55a3cb18ce8b38d58be12c60410c999
Gábor Tamás Tornai <gabo0127@edu.easj.dk>
2021-09-06 23:39:43 +0200

    Task 3

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,6 +10,30 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            
+
+            ClassRoom c1 = new ClassRoom();
+
+            c1.ClassName = "A.2134";
+            c1.ClassList = new List<Student>();
+            c1.Semester = new DateTime(2021, 08, 30);
+
+
+            Student s1= new Student($"Gabor",11,03);
+            Student s2 = new Student($"Dávid", 11, 21);
+            Student s3 = new Student($"Tony", 09, 03);
+            Student s4 = new Student($"George", 08, 18);
+            
+
+            c1.ClassList.Add(s1);
+            c1.ClassList.Add(s2);
+            c1.ClassList.Add(s3);
+            c1.ClassList.Add(s4);
+
+
+
+
+
         }
     }
 }
