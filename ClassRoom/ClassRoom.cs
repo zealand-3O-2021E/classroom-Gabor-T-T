a464c2fd6293f05d7e806642aed5dd43c312ba2c
Gábor Tamás Tornai <gabo0127@edu.easj.dk>
2021-09-06 22:56:03 +0200

    Second Task

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class ClassRoom
+    {
+        private string _className;
+        private List<Student> _classList;
+        private DateTime _semesterStart;
+
+        public ClassRoom()
+        {
+            ClassName = _className;
+            ClassList = _classList;
+            Semester = _semesterStart;
+        }
+
+        public string ClassName { get; set; }
+
+        public List<Student> ClassList { get; set; }
+
+        public DateTime Semester { get; set; }
+
+
+    }
+}
